e870a88507ec0dd454d16fab16773b98e475e335
zhuxiaolong-domi <1113953622@qq.com>
2020-12-27 19:16:49 +0800

    回退

--- Lab-Management-System-Backend/README.md
+++
@@ -1,5 +0,0 @@
-# LMS-backend
-
-## 软件项目管理后端
-
-### 拉下项目配置maven即可启动，测试程序在Controller中的HelloController中。运行**LmsApplication**即可。
--- Lab-Management-System-Backend/src/main/java/njust/lmsbackend/lms/DAO/UserDAO.java
+++
@@ -1,6 +0,0 @@
-package njust.lmsbackend.lms.DAO;
-import njust.lmsbackend.lms.POJO.UserPOJO;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserDAO extends JpaRepository<UserPOJO, String>{
-}
